9bfb031c62b75c33cc749c8ada93a58c9fd4da7a
start.vaadin.com <no-reply@vaadin.com>
2024-07-11 00:18:26 +0000

    Generated project

---
+++ src/main/java/com/grupo4/proyecto/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.grupo4.proyecto.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/grupo4/proyecto/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.grupo4.proyecto.services;
+
+import org.springframework.lang.NonNullApi;
